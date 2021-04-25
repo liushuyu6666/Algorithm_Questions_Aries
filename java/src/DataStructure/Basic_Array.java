60c914dd07a57d47362f1a629adc30fafc58ac86
liushuyu6666 <liushuyu6666@users.noreply.github.com>
2021-04-25 12:42:19 -0400

    add new data structure: array

---
+++ java/src/DataStructure/Basic_Array.java
@@ -0,0 +1,16 @@
+package DataStructure;
+
+import java.util.Arrays;
+
+public class Basic_Array {
+    public static void main(String args[]){
+
+        // 1, new
+        int[] array = new int[]{3, 1, 2, 2};
+
+        // 2, sort
+        Arrays.sort(array); // sort in-place
+        System.out.println("sorted array:");
+        for(int a:array) System.out.println(a);
+    }
+}
