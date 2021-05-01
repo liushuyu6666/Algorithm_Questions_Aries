73eb0c0fd58ecd19472b05471cb4218a0f1bb2a8
liushuyu6666 <liushuyu6666@users.noreply.github.com>
2021-05-01 13:12:32 -0400

    create ArrayList

---
+++ java/src/DataStructure/Basic_ArrayList.java
@@ -0,0 +1,14 @@
+package DataStructure;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Basic_ArrayList {
+    public static void main(String args[]){
+
+        // 1, new
+        List<Integer> list = Arrays.asList(1, 2, 3);
+        System.out.println(list);
+    }
+}
