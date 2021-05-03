acd86bf94b82c05ad1b4fc421ef5c70f4d775f24
liushuyu6666 <liushuyu6666@users.noreply.github.com>
2021-05-03 10:53:07 -0400

    update by a more general method

--- java/src/BinaryTree/Construct_Binary_Tree_from_Preorder_and_Postorder_Traversal.java
+++ java/src/BinaryTree/Construct_Binary_Tree_from_Preorder_and_Postorder_Traversal.java
@@ -8,18 +8,22 @@ public class Construct_Binary_Tree_from_Preorder_and_Postorder_Traversal {
         Stack<TreeNode> stack = new Stack<>();
         TreeNode root = new TreeNode(pre[0]);
         stack.push(root);
+
         int j = 0;
         for(int i = 1; i < pre.length; i++){
             TreeNode node = new TreeNode(pre[i]);
-            while(stack.peek().val == post[j]){
+            // determine to turn right
+            if(stack.peek().val == post[j]){
                 j++;
                 stack.pop();
-            }
-            if(stack.peek().left == null){
-                stack.peek().left = node;
+                while(stack.peek().val == post[j]){
+                    j++;
+                    stack.pop();
+                }
+                stack.peek().right = node;
             }
             else{
-                stack.peek().right = node;
+                stack.peek().left = node;
             }
             stack.push(node);
         }
