c89514ebade08f0ef3963ef21fa77cb09539b168
liushuyu6666 <liushuyu6666@users.noreply.github.com>
2021-05-02 21:01:39 -0400

    add subList

--- java/src/DataStructure/Basic_ArrayList.java
+++ java/src/DataStructure/Basic_ArrayList.java
@@ -8,7 +8,11 @@ public class Basic_ArrayList {
     public static void main(String args[]){
 
         // 1, new
-        List<Integer> list = Arrays.asList(1, 2, 3);
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
         System.out.println(list);
+
+        // 2, subList
+        List<Integer> subList = list.subList(0, 3);
+        System.out.println(subList);
     }
 }
